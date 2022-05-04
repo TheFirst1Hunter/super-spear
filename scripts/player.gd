51cfdb7a4598fd35eb35df5bce46d6df50e6b670
ahmed nateq <71846635+lucilf3r@users.noreply.github.com>
2022-05-04 18:21:07 +0300

    Add files via upload

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,13 @@
 extends KinematicBody
 
-export var speed:float = 10
-export var max_speed:float = 100
-export var mouse_sensitivity:float = 1
+export var speed:float = 40
+export var max_speed:float = 20
+export var mouse_sensitivity:float = 0.5
+const ACCEL_DEFAULT = 15
+const ACCEL_AIR = 1
+onready var accel = ACCEL_DEFAULT
+var gravity = 11
+var jump = 5
 
 var movement_direction:Vector3
 var spear_path:PackedScene = load("res://entites/spear.tscn")
@@ -11,43 +16,71 @@ onready var head:Spatial = $head
 onready var muzzle:Position3D = $head/muzzle
 onready var hand:Position3D = $hand/hand
 onready var ray_cast:RayCast=$head/RayCast
+onready var campivot = $head/Camera
+onready var mesh = $MeshInstance
+
+
+var cam_accel = 40
+var mouse_sense = 0.1
+var snap
+
+var angular_velocity = 30
+
+var direction = Vector3()
+var velocity = Vector3()
+var gravity_vec = Vector3()
+var movement = Vector3()
+
 
 func _ready():
-	pass
-
-func _input(event:InputEvent):
-	if not event is InputEventKey and not event is InputEventMouseMotion:
-		return
+	#hides the cursor
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
+	#mesh no longer inherits rotation of parent, allowing it to rotate freely
+	#mesh.set_as_toplevel(true)
+	
+func _input(event):
+	#get mouse input for camera rotation
 	if event is InputEventMouseMotion:
-		rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
-		head.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
+		rotate_y(deg2rad(-event.relative.x * mouse_sense))
+		head.rotate_x(deg2rad(-event.relative.y * mouse_sense))
 		head.rotation.x = clamp(head.rotation.x, deg2rad(-89), deg2rad(89))
 	
-	
-	if event.is_action_pressed("forward"):
-		movement_direction=-transform.basis.z
-		speed = max(speed+1,max_speed)
-	
-	if event.is_action_pressed("backward"):
-		movement_direction=transform.basis.z
-		speed = max(speed+1,max_speed)
-	
-	if event.is_action_pressed("right"):
-		movement_direction=transform.basis.x
-		speed = max(speed+1,max_speed)
-	
-	if event.is_action_pressed("left"):
-		movement_direction=-transform.basis.x
-		speed = max(speed+1,max_speed)
-	
-	if event.is_action_pressed("ui_accept"):
+	if Input.is_action_just_pressed("fire"):
 		shoot()
 
+func _process(delta):
+	pass
+func _physics_process(delta):
+	#get keyboard input
+	direction = Vector3.ZERO
+	var h_rot = global_transform.basis.get_euler().y
+	var f_input = Input.get_action_strength("backward") - Input.get_action_strength("forward")
+	var h_input = Input.get_action_strength("right") - Input.get_action_strength("left")
+	direction = Vector3(h_input, 0, f_input).rotated(Vector3.UP, h_rot).normalized()
+	
+	#jumping and gravity
+#	if is_on_floor():
+#		snap = -get_floor_normal()
+#		accel = ACCEL_DEFAULT
+#		gravity_vec = Vector3.ZERO
+#	else:
+#		snap = Vector3.DOWN
+#		accel = ACCEL_AIR
+#		gravity_vec += Vector3.DOWN * gravity * delta
+#
+#	if Input.is_action_just_pressed("jump") and is_on_floor():
+#		snap = Vector3.ZERO
+#		gravity_vec = Vector3.UP * jump
+	
+	#make it move
+	velocity = velocity.linear_interpolate(direction * speed, accel * delta)
+	if Input.is_action_just_pressed("dash"):
+		velocity = velocity.linear_interpolate(direction * speed * 4, accel * delta * 15)
+	movement = velocity + gravity_vec
+	
+	move_and_slide(movement, Vector3.UP)
 
-func _physics_process(delta:float):
-#	movement_direction.y=-0.5
-	move_and_slide(movement_direction*speed)
 
 
 func shoot():
